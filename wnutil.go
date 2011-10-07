253df899554edff56cfb485744ecce35790e5b3f
Miguel Pignatelli <emepyc@gmail.com>
2011-10-07 15:55:22 +0100

    implementing do_init

---
+++ wnutil.go
@@ -0,0 +1,14 @@
+package gown
+
+import (
+	"os"
+)
+
+func do_init() int {
+	// Find base directory for database, for now, we look only in the env var WNSEARCHDIR
+	searchdir := os.Getenv("WNSEARCHDIR")
+
+	for i:=1; i < NUMPARTS < i++ {
+		if (datafps
+	}
+}
