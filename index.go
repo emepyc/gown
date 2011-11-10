e93663aec45651cd51cb6feef642d6e754adb73d
Miguel Pignatelli <emepyc@gmail.com>
2011-11-10 17:16:50 +0000

    changed the indexMaps type

--- index.go
+++ index.go
@@ -15,21 +15,24 @@ const (
 	POS                  // 1
 	SYNSET_CNT           // 2
 	PTR_CNT              // 3
-	SENSE_CNT            // 4
-	TAGSENSE_CNT         // 5
-	SYNSET_OFFSET        // 6
+	SYMBOL               // 4
+	SENSE_CNT            // 5
+	TAGSENSE_CNT         // 6
+	SYNSET_OFFSET        // 7
 )
 
-// Error msgs
+// Error msgs -- may be refactored in another src file
 const (
 	_ = iota
 	UNKNOWN_WORD
+	SYNTACTIC_CATEGORY_TOO_LONG
+	LINE_TOO_LONG
 )
 
-const NINDEXRECS = 363000 // Current number of lines in index.* (wc -l index.*)
-const BUFFSIZE = 3072     // for reading lines in data
 const (
-	NUMPARTS = 4
+	NINDEXRECS = 363000 // Current number of lines in index.* (wc -l index.*)
+	BUFFSIZE = 3072 // Buffer for reading lines in data
+	NUMPARTS = 4 // noun, adj, verb, adverb
 	//	NUMFRAMES = 35
 )
 
@@ -38,22 +41,22 @@ var partnames []string = []string{"", "noun", "verb", "adj", "adv"}
 type indexInfo struct {
 	lemma        []byte
 	pos          byte // 'n', 'v', 'a', 'r' ... should we have these in a map? probably yes
-	offsets      []int64
+	p_cnt        int
+	ptr_symbols   [][]byte
 	tagsense_cnt int
+	offsets      []int64
 }
 
+// May be refactored in another source file
 type ERR_MSG int
 
+// May be refactored in another source file
 func (t ERR_MSG) Error() string {
 	return errMsg(int(t))
 }
 
-type indexMap map[string][]int64 // TODO: Profile in-memory indexing
-type tagSenseMap map[string]int
-type InMemIndex struct {
-	indexMaps    [NUMPARTS + 1]indexMap
-	tagSenseMaps [NUMPARTS + 1]tagSenseMap
-}
+type indexMap map[string]indexInfo // TODO: Profile in-memory indexing (better *indexInfo?)
+type indexMaps [NUMPARTS+1]indexMap
 
 type Indexer interface {
 	Lookup([]byte, int) ([]int64, error)
@@ -69,21 +72,26 @@ type WordNetDb struct {
 
 func errMsg(n int) string {
 	switch n {
-	case 1 :
+	case UNKNOWN_WORD :
 		return "UNKNOWN WORD"
+	case SYNTACTIC_CATEGORY_TOO_LONG:
+		return "SYNTACTIC CATEGORY TOO LONG IN INDEX FILE (expected values are 'n', 'v', 'a' or 'r'"
+	case LINE_TOO_LONG :
+		return "LINE TOO LONG"
 	default :
 		return "UNKNOWN ERROR MSG"
 	}
 	return "" // will not reach here
 }
 
-func (i *InMemIndex) Lookup(b []byte, pos int) ([]int64, error) {
-	m := i.indexMaps[pos]
-	offsets, ok := m[string(b)]
+
+func (i *indexMaps) Lookup(b []byte, pos int) ([]int64, error) {
+	m := i[pos] // returns an indexMap
+	lemma, ok := m[string(b)]
 	if !ok {
 		return nil, ERR_MSG(UNKNOWN_WORD)
 	}
-	return offsets, nil
+	return lemma.offsets, nil
 }
 
 func New() (*WordNetDb, error) {
@@ -108,23 +116,21 @@ func New() (*WordNetDb, error) {
 // TODO: Check for WordNet version. It is stated more or less at the beginning of the file
 func loadIndex(searchdir string) (Indexer, error) {
 	fmt.Fprintf(os.Stderr, "Reading Index (in memory)...\n") // TODO: Time this
-	index := InMemIndex{}
+	var index indexMaps
 
 	for i := 1; i <= NUMPARTS; i++ {
 		indexMap := make(indexMap, NINDEXRECS)
-		tagSenseMap := make(tagSenseMap, NINDEXRECS)
 		indexpath := fmt.Sprintf("%s/index.%s", searchdir, partnames[i]) // TODO: Make this portable
 		fmt.Fprintf(os.Stderr, "Processing index file %s\n", indexpath)
 		indexfh, err := os.Open(indexpath)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "WordNet library error: Can't open indexfile (%s)\n", indexpath)
 			return nil, err
 		}
 
 		defer func() {
 			e := indexfh.Close()
 			if e != nil {
-				fmt.Fprintf(os.Stderr, "Problem closing the file %s: %s\n", indexpath, e)
+				log.Fatal("Problem closing the file")
 			}
 		}()
 
@@ -135,12 +141,10 @@ func loadIndex(searchdir string) (Indexer, error) {
 			fmt.Fprintf(os.Stderr, "\r%d lines                 ", nlines)
 			line, isPrefix, err := bufindexfh.ReadLine()
 			if isPrefix {
-				fmt.Fprintf(os.Stderr, "Line too long reading file (%s)\n", indexpath)
-				os.Exit(1) // TODO: Exit?
+				return nil, ERR_MSG(LINE_TOO_LONG)
 			}
 			if err == io.EOF {
-				index.indexMaps[i] = indexMap
-				index.tagSenseMaps[i] = tagSenseMap
+				index[i] = indexMap
 				break
 			}
 			if err != nil {
@@ -149,10 +153,12 @@ func loadIndex(searchdir string) (Indexer, error) {
 			if line[0] == ' ' { // header line
 				continue
 			}
-			newIndexInfo := parseIndexLine(line)
+			newIndexInfo, err := parseIndexLine(line)
+			if err != nil {
+				return nil, err
+			}
 			key := string(newIndexInfo.lemma)
-			indexMap[key] = newIndexInfo.offsets
-			tagSenseMap[key] = newIndexInfo.tagsense_cnt
+			indexMap[key] = *newIndexInfo
 		}
 	}
 	fmt.Fprintf(os.Stderr, "Done\n")
@@ -176,40 +182,40 @@ func dataFh(searchdir string) (dataFiles, error) {
 	return datafps, nil
 }
 
-func parseIndexLine(l []byte) *indexInfo {
+func parseIndexLine(l []byte) (*indexInfo, error) {
 	//	fmt.Fprintf (os.Stderr, "Processing line:\n%s\n", l)
 	newIndexInfo := indexInfo{}
 	var err error
 	fields := bytes.Fields(l)
-	ptr_cnt, err := strconv.Atoi(string(fields[PTR_CNT]))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "I had a problem trying to convert '%s' to int\n", fields[PTR_CNT])
-		os.Exit(1)
-	}
 	newIndexInfo.lemma = fields[LEMMA]
 	//	newIndexInfo.pos, err = strconv.Atoui64(string(fields[POS]))
 	if len(fields[POS]) > 1 {
-		fmt.Fprintf(os.Stderr, "POS has to be 1 letter code ('n', 'v', 'a' or 'r') and I have %s\n", fields[POS])
-		os.Exit(1)
+		return nil, ERR_MSG(SYNTACTIC_CATEGORY_TOO_LONG)
 	}
 	newIndexInfo.pos = fields[POS][0]
 
+	ptr_cnt, err := strconv.Atoi(string(fields[PTR_CNT]))
+	if err != nil {
+		return nil, err
+	}
+
+	ptr_symbols := fields[SYMBOL:SYMBOL+ptr_cnt]
+	newIndexInfo.ptr_symbols = ptr_symbols
+
 	newIndexInfo.tagsense_cnt, err = strconv.Atoi(string(fields[TAGSENSE_CNT+ptr_cnt]))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "I had a problem trying to convert %s to int32\n", fields[TAGSENSE_CNT+ptr_cnt])
-		os.Exit(1)
+		return nil, err
 	}
-	offsets_strs := fields[(SYNSET_OFFSET + ptr_cnt):]
+	offsets_strs := fields[(SYNSET_OFFSET + ptr_cnt - 1):]
 	offsets := make([]int64, len(offsets_strs))
 	for i, offset := range offsets_strs {
 		offsets[i], err = strconv.Atoi64(string(offset))
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "I had a problem trying to convert the offset %s to int63\n", offset)
-			os.Exit(1) // log.Fatal?
+			return nil, err
 		}
 	}
 	newIndexInfo.offsets = offsets
-	return &newIndexInfo
+	return &newIndexInfo, nil
 }
 
 func main() {
