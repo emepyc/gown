7955e09fee1bd689793a2840aa0751a9e0e7aa3c
Miguel Pignatelli <emepyc@gmail.com>
2011-11-10 17:19:05 +0000

    the old search.go file has been renamed wnsearch, this is a wip from readLine.go

--- search.go
+++ search.go
@@ -1,224 +1,255 @@
 package gown
 
 import (
+	"errors"
 	"fmt"
-	"os"
-	"strings"
 	"bytes"
-	"encoding/hex"
+	"io"
+	"os"
+	"strconv"
+	"encoding/hex" // only for the error types
 )
 
-const (
-	DONT_KNOW  =  iota
-	DIRECT_ANT           // direct antonyms (cluster head)
-	INDIRECT_ANT         // indirect antonyms (similar)
-	PERTAINYM            // no antonyms or similars (pertainyms)
-)
+type Data os.File
 
-const (
-	ALLWORDS = 0       // print all words
-	SKIP_ANTS = 0      // skip printing antonyms in printsynset()
-	PRINT_ANTS = 1     // print antonyms in printsynset()
-	SKIP_MARKER = 0    // skip printing adjective marker
-	PRINT_MARKER = 1   // print adjective marker
-)
-
-const (
-	TRACEP = iota + 1  // traceptrs
-	TRACEC             // tracecoords()
-	TRACEI             // traceinherit()
-)
-
-const (
-	DEFOFF = iota
-	DEFON
-)
-
-const TMPBUFSIZE = 1024*10
-
-var (
-	prflag, sense, prlexid int
-	overflag int = 0              // set when output buffer overflows *** PROBABLY NOT NEEDED ***
-	searchbuffer []byte
-	lastholomero int              // keep track of last holo/meronym printed
-        tmpbuf []byte                 // general purpose printing buffer *** PROBABLY NOT NEEDED OR bufio ***
-	wdbuf []byte                  // general purpose word buffer *** PROBABLY NOT NEEDED OR bufio ***
-	msgbuf []byte                 // buffer for constructing error messages *** NOT NEEDED OR bufio ? ***
-)
-
-//type IndexDB struct {
-	// ???
-//}
+const BUFFSIZE = 10
 
 type lemma struct {
-	word []byte,
-	lex_id int, // 1-digit hexadecimal integer
+	word   []byte
+	lex_id int // 1-digit hexadecimal integer
+}
+
+type synsetPtr struct {
+	symbol    []byte
+	offset    int64
+	pos       byte
+	source    int
+	target    int
 }
 
 type dataData struct {
-	synset_offset int64, // Current byte offset in the file represented as an 8-digit dec integer
-	lex_filenum int, // 2-digit integer
-	ss_type byte, // n => NOUN, v => VERB, a => ADJECTIVE, s => ADJECTIVE SATELLITE, r => ADVERB
-	w_cnt int, // 2-digit hexadecimal integer
-	lemmas []*lemma
-	gloss []byte
-
+	synset_offset int64 // Current byte offset in the file represented as an 8-digit dec integer
+	lex_filenum   int   // 2-digit integer
+	ss_type       byte  // n => NOUN, v => VERB, a => ADJECTIVE, s => ADJECTIVE SATELLITE, r => ADVERB
+	w_cnt         int   // 2-digit hexadecimal integer
+	lemmas        []*lemma
+	p_cnt         int
+	ptrs          []*synsetPtr
+	gloss         []byte
 }
 
-type Query struct {
-	option []byte, // user's search request
-	search int, // search to pass findtheinfo()
-	pos int, // part-of-speech to pass findtheinfo()
-	sense int, // Perform search on sense number # only
-	rel []byte // Relation
-	// helpmsgidx int, // index into help message table
-	// label []char, // text for search header message
-}
-
-var relNameSyn = map[string][]string {
-	"ants" : []string{"!"},
-	"hype" : []string{"@"},
-	"inst" : []string{"@i"},
-	"hypes" : []string{"@", "@i"},
-}
-
-// type Index struct {
-// 	idxoffset int64 // byte offset of entry in index file
-// 	wd []byte // word string
-// 	pos []byte // part of speech
-// 	sense_cnt int // sense (collins) count
-// 	off_cnt int // number of offsets
-// 	tagget_cnt int // number of senses that are tagged
-// 	offset uint64 // offsets of synsets containing word
-// 	ptruse_cnt int // number of pointers used
-// 	ptruse int // pointers used. ***IN wn.h THIS IS A POINTER TO A INT***
-// }
-
-type Synset struct {
-	hereiam int64 // current file position
-	ssypte int // type of ADJ synset
-	fnum int // file number that synset comes from
-	pos []byte // part of speech
-	wcount int // number of words in synset
-	words [][]byte // words in synset
-	lexid int //unique id in lexicographer file *** In wn.h this is a pointer to a int ***
-	wnsns int // sense number in wordnet *** In wn.h this is a pointer to a int ***
-	whichword int // which word in synsest we're looking for
-	ptrcount int // number of pointers
-	ptrtyp int // pointer types *** In wn.h this is a pointer to a int ***
-	ptroff int64 // pointer offsets *** In wn.h this is a pointer to a long ***
-	ppos int // pointer part of speech *** In wn.h this is a pointer to a int ***
-	pto int // pointer 'to' fields *** In wn.h this is a pointer to a int ***
-	pfrm int // pointer 'from' fields *** In wn.h this is a pointer to a int ***
-	fcount int // number of verb frames
-	frmid int // frame numbers *** In wn.h this is a pointer to a int ***
-	frmto int // frame 'to' fields *** In wn.h this is a pointer to a int ***
-	defn []byte // synset gloss (definition)
-	key uint32 // unique synset key
-
-/* these fields are used if a data structure is returned
- instead of a text buffer */
-
-	nextss *Synset // ptr to next synset containing searchword
-	nextform *Synset // ptr to list of synsets for alternate spelling of wordform
-
-	searchtype int // type of search performed
-	ptrlist []*Synset // ptr to synset list result of search
-	headword []byte // if pos is "s", this is cluster head word
-	headsense int16 // sense number of headword
-}
-
-type SnsIndex struct {
-	sensekey []byte // sense key
-	word []byte // word string
-	loc int64 // synset offset
-	wnsense int // WordNet sense number
-	tag_cnt int // number of semantic tags to sense
-	nextsi *SnsIndex // ptr to next sense index entry
-}
-
-type SearchResults struct {
-	SenseCount [MAX_FORMS]byte // number of senses word form has
-	OutSenseCount [MAX_FORMS]byte // number of senses printed for word form
-	numforms int // number of word forms searchword has
-	printcnt int // number of senses printed by search
-	searchbuf []byte // buffer containing formatted results *** change to bufio.New ? ***
-	searchds *Synset // data structure containing search results
-}
-
-// Find word in index file and return parsed entry in data structure
-// Input word must be exact match of string in database (case insensitive)
-func (wndb *WordNetDb) indexOffsetLookup (query Query) []int64 {
-	lword := strToLower(query.option)
-	offsets := wndb.Index.Lookup(lword, query.pos)
-
-	if query.sense != 0 {
-		offsets = offsets[:query.sense]
+func (wndb *WordNetDb) dataLookup(fh *os.File, offset int64) ([]byte, error) {
+	_, err := fh.Seek(offset, os.SEEK_SET)
+	if err != nil {
+		return nil, err
 	}
 
-	return offsets
+	buffer := make([]byte, BUFFSIZE) // initial size of the buffer is 3kb
+	line := make([]byte, 0, BUFFSIZE)
+	prevLen := 0
+	for {
+		prevLen = len(line)
+		n, err := fh.Read(buffer) // we read the next 3kb (or less)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		line = append(line, buffer[:]...)
+		until := bytes.IndexByte(buffer, '\n')
+		if until > 0 { // We have a full line
+			return line[:prevLen+until], nil
+		}
+		if err == io.EOF || n < BUFFSIZE {
+			return line, nil
+		}
+	}
+	return nil, ERR_MSG(UNREACHABLE_CODE)
 }
 
-
-func (wndb *WordNetDb) QuerySense (query *Query) [][]byte {
-	fmt.Fprintf(os.Stderr, "(QuerySense) STRING=%s#%d#%d#%d\n", query.option, query.search, query.pos, query.sense) // Only for dubugging
-
-	lword := strToLower(query.option)
-
-	rtn := make([][]byte, 0, 10) // 10?
-
-	if query.sense != 0 {
-		if query.rel == nil {
-			fmt.Fprintf(os.Stderr, "(QuerySense) Relation required\n")
-			os.Exit(1) // return err?
-		}
-		if rel, ok := relSymName[string(query.rel)]; ok {
-			query.rel = rel
-		} else if _, ok := relNameSym[string(query.rel)]; ok || string(query.rel) == "glos" || string(query.rel) == "syns" {
-		} else {
-			fmt.Fprintf(os.Stderr, "(QuerySense) Bad relation: %s\n", query.rel)
-			os.Exit(1) // return err?
-		} 
-
-		offsets = wndb.indexOffsetLookup(lword, query.pos, query.sense)
-		offset = offset[0]
-		dataLine := dataLookup(query.pos, offset)
-
-		if rel eq "glos" {
-			lastIndex := bytes.LastIndex(dataLine, "| ")
-			// if lastIndex == -1??
-			rtn = append(rtn, dataLine[lastIndex+1:])
-		} else if rel eq "syns" {
-			allSenses = getAllSenses(query.pos, offset)
+func (wndb *WordNetDb) GetRelation(pos int , offset int64, symbol []byte) ([]synsetPtr, error) {
+	fh := wndb.Data[pos]
+	osFh, ok := fh.(*os.File)
+	if !ok {
+		return nil, ERR_MSG(NOT_A_VALID_FILE_POINTER)
+	}
+	dataLine, err := wndb.dataLookup(osFh, offset)
+	if err != nil {
+		return nil, err
+	}
+	ptrs := make([]synsetPtr, 0, 2) // larger cap?
+	for {
+		posInLine := bytes.Index(dataLine, symbol)
+		if posInLine < 0 { // no occurrence
+			return ptrs, nil
 		}
 		
+		ptr, newPos, err := nextPtr(dataLine, posInLine)
+		if err != nil {
+			return nil, err
+		}
+		dataLine = dataLine[newPos:]
+		ptrs = append(ptrs, *ptr)
 	}
+	return nil, ERR_MSG(UNREACHABLE_CODE)
 }
 
-func parseDataLine(dataLine []byte) *dataData, os.Error {
+func parseDataLine(dataLine []byte) (*dataData, error) {
 	data := &dataData{}
+	var err error
+
 	// gloss
-	lastIndex := bytes.LastIndex(dataLine, "| ")
-	data.gloss = dataLine[lastIndex:]
-	return data
-}
-
-func (wndb *WordNetDb) getAllSenses(dataLine []byte) [][]byte {
-	fmt.Fprintf(os.Stderr, "(getAllSenses) line=%s\n", dataLine) // debugging
-
-	rtn := make([][]byte, 0, 10) // 10?
-	dataParts := bytes.SplitN(dataLine, []byte{' '}, 5)
-	w_cnt, err := x2i(dataParts[3])
-
-	words := make([][]byte, w_cnt)
-	for i:=0; i<w_cnt; i++ {
-		words[i]
+	lastIndex := bytes.LastIndex(dataLine, []byte{'|', ' '})
+	if lastIndex == -1 {
+		return nil, errors.New(`No gloss delimiter found "| " in line`)
 	}
+	data.gloss = dataLine[lastIndex+2:]
+
+	// synset_offset  --- not used
+	synsetOffsetBytes := dataLine[:8]
+	fmt.Printf("synsetOffsetBytes: [%s]\n", synsetOffsetBytes)
+	synset_offset, err := strconv.Atoi64(string(synsetOffsetBytes))
+	if err != nil {
+		return nil, err
+	}
+	data.synset_offset = synset_offset
+
+	// lex_filenum  --- not used
+	lexFilenumBytes := dataLine[9:11]
+	fmt.Printf("lexFilenumBytes: [%s]\n", lexFilenumBytes)
+	lexFilenum, err := strconv.Atoi(string(lexFilenumBytes))
+	if err != nil {
+		return nil, err
+	}
+	data.lex_filenum = lexFilenum
+
+	// ss_type
+	switch ss_type := dataLine[12]; {
+	case ss_type == 'n' ||
+		ss_type == 'v' ||
+		ss_type == 'a' ||
+		ss_type == 's' ||
+		ss_type == 'r':
+		data.ss_type = ss_type
+	default:
+		return nil, errors.New(fmt.Sprintf("Invalid ss_type: %c\n", ss_type))
+	}
+
+	// w_cnt
+	w_cntBytes := dataLine[14:16]
+	fmt.Printf("w_cntBytes: [%s]\n", w_cntBytes)
+	w_cnt, err := x2i(w_cntBytes)
+	if err != nil {
+		return nil, err
+	}
+	data.w_cnt = w_cnt
+
+	// lemmas
+	lemmas := make([]*lemma, w_cnt)
+	fromPos := 17
+	for i := 0; i < w_cnt; i++ {
+		nextLemma, posInLine, err := nextSense(dataLine, fromPos)
+		if err != nil {
+			return nil, err
+		}
+		fromPos = posInLine
+		lemmas[i] = nextLemma
+	}
+	data.lemmas = lemmas
+
+	// p_cnt
+	p_cntBytes := dataLine[fromPos:fromPos+3]
+	fmt.Printf("p_cnt: [%s]\n", p_cntBytes)
+	p_cnt, err := strconv.Atoi(string(p_cntBytes))
+	if err != nil {
+		return nil, err
+	}
+	data.p_cnt = p_cnt
+
+	// ptrs
+	ptrs := make([]*synsetPtr, p_cnt)
+	fromPos = fromPos + 4
+	for i := 0; i < p_cnt; i++ {
+		nextPtr, posInLine, err := nextPtr(dataLine, fromPos)
+		if err != nil {
+			return nil, err
+		}
+		fromPos = posInLine
+		ptrs[i] = nextPtr
+	}
+	data.ptrs = ptrs
+
+	//frames: In data.verb only, a list of numbers corresponding to the generic verb sentence frames for word s in the synset. frames is of the form:
+
+	return data, nil
 }
 
-func x2i(src []byte) (int, os.Error) {
-	if len(src) > 2 {
-		return 0, hex.InvalidHexCharError()
+func nextSense(line []byte, pos int) (*lemma, int, error) {
+	lemma := &lemma{}
+	acc := make([]byte, 30)
+	from := pos
+	for i, ch := range line[pos:] {
+		if ch == ' ' {
+			lemma.word = acc
+			from += i + 1
+			break
+		}
+		acc[i] = ch
+	}
+	xval := line[from]
+	ival, ok := fromHexChar(xval)
+	if !ok {
+		return nil, 0, errors.New(fmt.Sprintf("Invalid hex byte (%c)", xval))
+	}
+	lemma.lex_id = int(ival)
+
+	return lemma, (from + 2), nil
+}
+
+func nextPtr(line []byte, pos int) (*synsetPtr, int, error) {
+	ptr := &synsetPtr{}
+	acc := make([]byte, 0, 2)
+	from := pos
+	for i, ch := range line[pos:] {
+		if ch == ' ' {
+			ptr.symbol = acc
+			from += i + 1
+			break
+		}
+		acc = append(acc, ch) // at most 2 digits in symbol
+	}
+	offsetBytes := line[from:from+8]
+	offset, err := strconv.Atoi64(string(offsetBytes))
+	if err != nil {
+		return nil, 0, err
+	}
+	ptr.offset = offset
+	from = from + 9
+
+	ptrpos := line[from]
+	ptr.pos = ptrpos
+
+	from = from+2
+	sourceBytes := line[from:from+2]
+	source, err := x2i(sourceBytes)
+	if err != nil {
+		return nil, 0, err
+	}
+	ptr.source = source
+
+	from = from+2
+	targetBytes := line[from:from+2]
+	target, err := x2i(targetBytes)
+	if err != nil {
+		return nil, 0, err
+	}
+	ptr.target = target
+
+	return ptr, from + 3, nil
+}
+
+// Utility function to convert a 2-digit hexadecimal number to int
+func x2i(src []byte) (int, error) {
+	if len(src) != 2 {
+		return 0, errors.New(fmt.Sprintf("Only 2-bytes hexadecimal integers allowed (passed [%s])\n", src))
 	}
 	d1, ok := fromHexChar(src[0])
 	if !ok {
@@ -232,7 +263,7 @@ func x2i(src []byte) (int, os.Error) {
 	return val, nil
 }
 
-fromHexChar(c byte) (byte, bool) {
+func fromHexChar(c byte) (byte, bool) {
 	switch {
 	case '0' <= c && c <= '9':
 		return c - '0', true
@@ -242,17 +273,5 @@ fromHexChar(c byte) (byte, bool) {
 		return c - 'A' + 10, true
 	}
 	return 0, false
-}
-
-/* Convert to lowercase and remove trailing adjective marker if found */
-func strToLower(str []byte) []byte {
-	ret := make ([]byte, len(str))
-	for i, ch := range str {
-		if ch == '(' {
-			return bytes.ToLower(ret)
-		}
-		ret[i] = ch
-	}
-	return bytes.ToLower(ret)
 }
 
